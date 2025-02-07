0c4985773876ecac40de12521f84a86e7965cafa
DanielMiguelez <danielmiguelez1993@gmail.com>
2025-02-07 12:55:56 +0100

    admin added

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {authentication} = require ("../middleware/authentication")
+const {authentication, isAdmin} = require ("../middleware/authentication")
 
 const ProductController = require("../controllers/ProductController");
 
 router.post("/createProduct",authentication,ProductController.productCreate)
-router.put("/productUpdate/:id", ProductController.productUpdate)
-router.delete("/destroyProduct/:id", ProductController.destroyProduct)
+router.put("/productUpdate/:id",authentication, ProductController.productUpdate)
+router.delete("/destroyProduct/:id",authentication,isAdmin, ProductController.destroyProduct)
 router.get("/getAllProductsWithCategories", ProductController.getAllProductsWithCategories)
 router.get("/getProductById/:id", ProductController.getProductById)
 router.get("/getProductByName/:name", ProductController.getProductByName)
