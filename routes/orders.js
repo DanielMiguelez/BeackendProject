a203ce450d9e0e0a5301623337d3d5d487c17051
DanielMiguelez <danielmiguelez1993@gmail.com>
2025-02-13 12:38:29 +0100

    last commit

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const {authentication, isAdmin} = require ("../middleware/authentication")
 
 const OrderController = require ("../controllers/OrderController");
 
-router.post("/createOrder", OrderController.createOrder)
+router.post("/createOrder", authentication, OrderController.createOrder)
 router.get("/getOrderById/:id", OrderController.getOrderById)
 router.get("/getOrders", OrderController.getOrders)
 
